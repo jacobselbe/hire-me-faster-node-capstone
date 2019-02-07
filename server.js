945484ddb8ca1db461e4077f20cdaa9095439470
Jacob Selbe <jalxselbe@gmail.com>
2019-02-07 17:50:46 -0600

    Setup tests and deployment
    Mocha, chai, and chai-http were installed as dependencies for testing. Basic test implemented to test server connection. Tavis Cl and heroku set up for continuous integration.

--- server.js
+++ server.js
@@ -1,5 +1,74 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());//do I need this?
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+let server;
+
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app
+            .listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve(server);
+            })
+            .on("error", err => {
+                reject(err);
+            });
+    });
+}
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log("Closing server");
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+//remember change runServer and closeServer when you integrate mongoose
+
+// function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+//     return new Promise((resolve, reject) => {
+//         mongoose.connect(databaseUrl, err => {
+//             if (err) {
+//                 return reject(err);
+//             }
+//             server = app.listen(port, () => {
+//                 console.log(`Your app is listening on port ${port}`);
+//                 resolve();
+//             })
+//                 .on('error', err => {
+//                     mongoose.disconnect();
+//                     reject(err);
+//                 });
+//         });
+//     });
+// }
+
+// function closeServer() {
+//     return mongoose.disconnect().then(() => {
+//         return new Promise((resolve, reject) => {
+//             console.log('Closing server');
+//             server.close(err => {
+//                 if (err) {
+//                     return reject(err);
+//                 }
+//                 resolve();
+//             });
+//         });
+//     });
+// }
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
